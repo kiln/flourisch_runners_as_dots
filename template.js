830047e629e509e9d2c8f1c48fbb8dcf442d15d5
malte hildebrand <malte.hildebrand@gmail.com>
2017-08-03 14:06:21 +0200

    initial commit

---
+++ template.js
@@ -0,0 +1,2 @@
+!function(t){"use strict";function e(){}function a(){}var i={},n={example_state_property:25};t.data=i,t.state=n,t.update=e,t.draw=a}(this.template=this.template||{});
+//# sourceMappingURL=template.js.map
